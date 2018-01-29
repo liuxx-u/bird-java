80bf065ecd6d607057cccb25675f62811f7d967d
liuxx <381334617@qq.com>
2018-01-29 15:58:12 +0800

    Number类型的校验方法添加对Null的过滤

--- bird-core/src/main/java/com/bird/core/Check.java
+++ bird-core/src/main/java/com/bird/core/Check.java
@@ -11,8 +11,9 @@ public final class Check {
 
     /**
      * 校验参数不能为空
+     *
      * @param object 参数
-     * @param name 参数名
+     * @param name   参数名
      */
     public static final void NotNull(Object object, String name) {
         if (object.equals(null)) {
@@ -22,35 +23,38 @@ public final class Check {
 
     /**
      * 校验字符串不能为空白
+     *
      * @param value 参数
-     * @param name 参数名
+     * @param name  参数名
      */
-    public static final void NotEmpty(String value, String name){
-        if(StringUtils.isBlank(value)){
+    public static final void NotEmpty(String value, String name) {
+        if (StringUtils.isBlank(value)) {
             throw new ArgumentException(name + "can not be null.");
         }
     }
 
     /**
      * 校验参数必须大于某值
-     * @param value 参数
+     *
+     * @param value  参数
      * @param target 目标值
-     * @param name 参数名
+     * @param name   参数名
      */
     public static final void GreaterThan(Long value, Long target, String name) {
-        if (value <= target) {
+        if (value == null || value <= target) {
             throw new ArgumentException(name + "must greater than" + target);
         }
     }
 
     /**
      * 校验参数必须小于某值
-     * @param value 参数
+     *
+     * @param value  参数
      * @param target 目标值
-     * @param name 参数名
+     * @param name   参数名
      */
-    public static final void LessThan(Long value,Long target,String name){
-        if (value >= target) {
+    public static final void LessThan(Long value, Long target, String name) {
+        if (value == null || value >= target) {
             throw new ArgumentException(name + "must less than" + target);
         }
     }
