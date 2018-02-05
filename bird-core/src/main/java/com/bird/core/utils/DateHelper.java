c9812236985aa2c9b9243b80884afc562ded338d
liuxx <381334617@qq.com>
2018-02-05 17:50:07 +0800

    1、bird-core中移除jcommander包；2、重构DateHelper方法命名

--- bird-core/src/main/java/com/bird/core/utils/DateHelper.java
+++ bird-core/src/main/java/com/bird/core/utils/DateHelper.java
@@ -10,17 +10,36 @@ import java.util.Date;
  * Created by liuxx on 2017/6/27.
  */
 public final class DateHelper {
+
+    /**
+     * 字符串转日期
+     * @param strDate 时间字符串
+     * @param pattern 模式
+     * @return
+     * @throws ParseException
+     */
     public static Date parse(String strDate, String pattern)
             throws ParseException {
         return org.apache.commons.lang.StringUtils.isBlank(strDate) ? null : new SimpleDateFormat(
                 pattern).parse(strDate);
     }
 
-    public static String getCurrentDateString(String pattern) {
-        return getDateString(new Date(),pattern);
+    /**
+     * 获取当前时间的字符串值
+     * @param pattern 模式
+     * @return
+     */
+    public static String strNow(String pattern) {
+        return format(new Date(),pattern);
     }
 
-    public static String getDateString(Date date, String pattern) {
+    /**
+     * 日期转字符串
+     * @param date 日期
+     * @param pattern 模式
+     * @return
+     */
+    public static String format(Date date, String pattern) {
         if (date == null) return null;
         if (StringUtils.isBlank(pattern)) {
             pattern = "yyyy-MM-dd HH:mm:ss";
--- bird-core/src/main/java/com/bird/core/utils/DozerHelper.java
+++ bird-core/src/main/java/com/bird/core/utils/DozerHelper.java
@@ -1,145 +1,25 @@
 package com.bird.core.utils;
 
-import com.beust.jcommander.internal.Lists;
 import org.dozer.DozerBeanMapper;
 import org.springframework.stereotype.Component;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 /**
- * Created by panda on 2017/6/14.
+ * Created by liuxx on 2017/6/14.
  */
 @Component
 public class DozerHelper  extends DozerBeanMapper {
 
-    public <T> List<T> mapList(Collection sourceList, Class<T> destinationClass)
-    {
-        List destinationList = Lists.newArrayList();
+    public <T> List<T> mapList(Collection sourceList, Class<T> destinationClass){
+        List destinationList = new ArrayList();
         for (Iterator i$ = sourceList.iterator(); i$.hasNext(); ) { Object sourceObject = i$.next();
             Object destinationObject = map(sourceObject, destinationClass);
             destinationList.add(destinationObject);
         }
         return destinationList;
-    }
-    /**
-     * 日期转换成字符串
-     * @param date
-     * @return str
-     */
-    public static String DateToStr(Date date) {
-
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        String str = format.format(date);
-        return str;
-    }
-
-    /**
-     * 字符串转换成日期
-     * @param str
-     * @return date
-     */
-    public static Date StrToDate(String str) {
-
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = null;
-        try {
-            date = format.parse(str);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return date;
-    }
-
-    /**
-     * 给指定定时间加上需要的天数
-     * @param date
-     * @param day
-     * @return
-     */
-    public static String DateAddDayStr(Date date,int day){
-        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
-        Calendar   calendar   =   new   GregorianCalendar();
-        calendar.setTime(date);
-        calendar.add(Calendar.DAY_OF_MONTH, day);
-        return sf.format(calendar.getTime());
-    }
-    /**
-     * 给指定定时间加上需要的天数
-     * @param date
-     * @param day
-     * @return
-     */
-    public static Date DateAddDayDate(Date date,int day){
-        Calendar   calendar   =   new   GregorianCalendar();
-        calendar.setTime(date);
-        calendar.add(Calendar.DAY_OF_MONTH, day);
-        return calendar.getTime();
-    }
-    /**
-     * 两个时间之间相差距离多少天
-     * @param str1 时间参数 1：
-     * @param str2 时间参数 2：
-     * @return 相差天数
-     */
-    public static long getDistanceDays(String str1, String str2) {
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        Date one;
-        Date two;
-        long days=0;
-        try {
-            one = df.parse(str1);
-            two = df.parse(str2);
-            long time1 = one.getTime();
-            long time2 = two.getTime();
-            long diff ;
-            if(time1<time2) {
-                diff = time2 - time1;
-            } else {
-                diff = time1 - time2;
-            }
-            days = diff / (1000 * 60 * 60 * 24);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return days;
-    }
-
-    /**
-     * 两个时间相差距离多少天多少小时多少分多少秒
-     * @param str1 时间参数 1 格式：1990-01-01 12:00:00
-     * @param str2 时间参数 2 格式：2009-01-01 12:00:00
-     * @return long[] 返回值为：{天, 时, 分, 秒}
-     */
-    public static long[] getDistanceTimes(String str1, String str2) {
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date one;
-        Date two;
-        long day = 0;
-        long hour = 0;
-        long min = 0;
-        long sec = 0;
-        try {
-            one = df.parse(str1);
-            two = df.parse(str2);
-            long time1 = one.getTime();
-            long time2 = two.getTime();
-            long diff ;
-            if(time1<time2) {
-                diff = time2 - time1;
-            } else {
-                diff = time1 - time2;
-            }
-            day = diff / (24 * 60 * 60 * 1000);
-            hour = (diff / (60 * 60 * 1000) - day * 24);
-            min = ((diff / (60 * 1000)) - day * 24 * 60 - hour * 60);
-            sec = (diff/1000-day*24*60*60-hour*60*60-min*60);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        long[] times = {day, hour, min, sec};
-        return times;
     }
 }
