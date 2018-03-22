3c874f974dacf458666e342f106e68d534f1efd7
liuxx <381334617@qq.com>
2018-03-22 15:15:00 +0800

    将示例service与web移入samples项目中

--- bird-service-scheduler/bird-service-scheduler-api/src/main/java/com/bird/service/scheduler/SchedulerLogService.java
+++
@@ -1,7 +0,0 @@
-package com.bird.service.scheduler;
-
-import com.bird.service.common.service.IService;
-import com.bird.service.scheduler.model.SchedulerLog;
-
-public interface SchedulerLogService extends IService<SchedulerLog> {
-}
