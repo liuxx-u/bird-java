474704cee525471cb2af15624e86ff1c965c706f
liuxx <381334617@qq.com>
2018-04-17 14:58:47 +0800

    修改工具类

--- bird-core/src/main/java/com/bird/core/utils/NumberHelper.java
+++ bird-core/src/main/java/com/bird/core/utils/NumberHelper.java
@@ -1,12 +1,14 @@
 package com.bird.core.utils;
 
+
+import org.apache.commons.lang3.math.NumberUtils;
 import java.math.BigDecimal;
 
 /**
  * @author liuxx
  * @date 2018/4/10
  */
-public class NumberHelper {
+public final class NumberHelper extends NumberUtils {
 
     public static final BigDecimal getSafety(BigDecimal value){
         if(value == null)return BigDecimal.ZERO;
