9e365cbd7fd12ac7c11e74ec17834d901f671fdf
liuxx <381334617@qq.com>
2018-05-21 19:01:10 +0800

    添加Number工具方法

--- bird-core/src/main/java/com/bird/core/utils/NumberHelper.java
+++ bird-core/src/main/java/com/bird/core/utils/NumberHelper.java
@@ -10,8 +10,56 @@ import java.math.BigDecimal;
  */
 public final class NumberHelper extends NumberUtils {
 
-    public static final BigDecimal getSafety(BigDecimal value){
-        if(value == null) {
+    /**
+     * 是否是正数
+     * @param number 数字
+     * @return
+     */
+    public static final boolean isPositive(Number number) {
+        return greaterThan(number, 0);
+    }
+
+    /**
+     * 是否不是正数
+     * @param number 数字
+     * @return
+     */
+    public static final boolean isNotPositive(Number number){
+        return !isPositive(number);
+    }
+
+    /**
+     * 是否大于
+     *
+     * @param number 数字
+     * @param target 目标值
+     * @return
+     */
+    public static final boolean greaterThan(Number number, Number target) {
+        if (number == null) return false;
+        return number.doubleValue() > target.doubleValue();
+    }
+
+    /**
+     * 是否小于
+     *
+     * @param number 数字
+     * @param target 目标值
+     * @return
+     */
+    public static final boolean lessThan(Number number, Number target) {
+        if (number == null) return false;
+        return number.doubleValue() < target.doubleValue();
+    }
+
+    /**
+     * 获取BigDecimal,value为null时,返回BigDecimal.ZERO
+     *
+     * @param value
+     * @return
+     */
+    public static final BigDecimal getSafety(BigDecimal value) {
+        if (value == null) {
             return BigDecimal.ZERO;
         }
         return value;
