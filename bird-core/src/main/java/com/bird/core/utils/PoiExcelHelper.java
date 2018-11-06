8e185531032d62be00e8896bdb9d9507c4b0ea19
liuxx <381334617@qq.com>
2018-11-06 16:17:50 +0800

    添加导出Excel工具方法

--- bird-core/src/main/java/com/bird/core/utils/PoiExcelHelper.java
+++ bird-core/src/main/java/com/bird/core/utils/PoiExcelHelper.java
@@ -1,22 +1,20 @@
 package com.bird.core.utils;
 
+import com.bird.core.exception.UserFriendlyException;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.DecimalFormat;
+import java.util.*;
 
 /**
  * Poi操作excel工具类
@@ -26,12 +24,14 @@ import java.util.Map;
  */
 public class PoiExcelHelper {
 
+    private static DecimalFormat DECIMAL_FORMATE = new DecimalFormat("0");
+
     /**
      * 读取本地EXCEL文件
      *
      * @param path   文件路径
      * @param config 头配置
-     * @return
+     * @return excel数据
      */
     public static List<Map<String, Object>> read(String path, Map<String, String> config) throws IOException {
         return read(path, config, 0, 0);
@@ -43,7 +43,7 @@ public class PoiExcelHelper {
      * @param path        文件路径
      * @param config      头配置
      * @param headerIndex 头所在的行
-     * @return
+     * @return excel数据
      */
     public static List<Map<String, Object>> read(String path, Map<String, String> config, Integer headerIndex) throws IOException {
         return read(path, config, 0, headerIndex);
@@ -56,7 +56,7 @@ public class PoiExcelHelper {
      * @param config      头配置
      * @param sheetIndex  sheet序号
      * @param headerIndex 头所在的行
-     * @return
+     * @return excel数据
      */
     public static List<Map<String, Object>> read(String path, Map<String, String> config, Integer sheetIndex, Integer headerIndex) throws IOException {
         InputStream stream = null;
@@ -80,7 +80,7 @@ public class PoiExcelHelper {
      *
      * @param uri    url地址
      * @param config 头配置
-     * @return
+     * @return excel数据
      */
     public static List<Map<String, Object>> readUrl(String uri, Map<String, String> config) throws IOException {
         return readUrl(uri, config, 0, 0);
@@ -92,7 +92,7 @@ public class PoiExcelHelper {
      * @param uri         url地址
      * @param config      头配置
      * @param headerIndex 头所在的行
-     * @return
+     * @return excel数据
      */
     public static List<Map<String, Object>> readUrl(String uri, Map<String, String> config, Integer headerIndex) throws IOException {
         return readUrl(uri, config, 0, headerIndex);
@@ -105,7 +105,7 @@ public class PoiExcelHelper {
      * @param config      头配置
      * @param sheetIndex  sheet序号
      * @param headerIndex 头所在的行
-     * @return
+     * @return excel数据
      */
     public static List<Map<String, Object>> readUrl(String uri, Map<String, String> config, Integer sheetIndex, Integer headerIndex) throws IOException {
         InputStream stream = null;
@@ -136,7 +136,7 @@ public class PoiExcelHelper {
      * @param config      表头与Key映射
      * @param sheetIndex  sheet序号
      * @param headerIndex header行序号
-     * @return
+     * @return excel数据
      */
     public static List<Map<String, Object>> read(InputStream stream, Map<String, String> config, Integer sheetIndex, Integer headerIndex) {
         List<Map<String, Object>> result = new ArrayList<>();
@@ -156,9 +156,8 @@ public class PoiExcelHelper {
                 if (line == null) continue;
                 result.add(line);
             }
-        } catch (InvalidFormatException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            wb.close();
+        } catch (InvalidFormatException | IOException e) {
             e.printStackTrace();
         }
 
@@ -166,11 +165,54 @@ public class PoiExcelHelper {
     }
 
     /**
+     * 导出Excel
+     *
+     * @param list      数据源
+     * @param config    表头与Key映射
+     * @param sheetName 工作表的名称
+     * @param out       导出流
+     */
+    public static void listToExcel(List<Map> list, LinkedHashMap<String, String> config, String sheetName, OutputStream out) {
+        Integer sheetSize = 65535;
+
+        if (CollectionUtils.isEmpty(list)) {
+            throw new UserFriendlyException("数据源中没有任何数据");
+        }
+        Workbook wb = new HSSFWorkbook();
+        try {
+            //因为2003的Excel一个工作表最多可以有65536条记录，除去列头剩下65535条
+            //所以如果记录太多，需要放到多个工作表中，其实就是个分页的过程
+            //1.计算一共有多少个工作表
+            double sheetNum = Math.ceil(list.size() / 65535d);
+            if (sheetNum == 1) {
+                Sheet sheet = wb.createSheet(sheetName);
+                fillSheet(sheet, list, config, 0, list.size() - 1);
+            } else {
+                //2.创建相应的工作表，并向其中填充数据
+                for (int i = 0; i < sheetNum; i++) {
+                    Sheet sheet = wb.createSheet(sheetName + (i + 1));
+
+                    //获取开始索引和结束索引
+                    int firstIndex = i * sheetSize;
+                    int lastIndex = Math.min((i + 1) * sheetSize - 1, list.size() - 1);
+                    //填充工作表
+                    fillSheet(sheet, list, config, firstIndex, lastIndex);
+                }
+            }
+
+            wb.write(out);
+            wb.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
      * 获取表头对应的字段名称
      *
-     * @param sheet
-     * @param config
-     * @return
+     * @param sheet  sheet页
+     * @param config 头信息配置
+     * @return 字段集合
      */
     private static List<String> getHeadKeys(Sheet sheet, Map<String, String> config, Integer headerIndex) {
         if (sheet == null || NumberHelper.isNotPositive(sheet.getLastRowNum())) return null;
@@ -191,10 +233,10 @@ public class PoiExcelHelper {
     /**
      * 读取每一行的数据
      *
-     * @param row
-     * @param headKeys
-     * @param evaluator
-     * @return
+     * @param row       行数据
+     * @param headKeys  keys
+     * @param evaluator 公式计算器
+     * @return 行数据
      */
     private static Map<String, Object> readLine(Row row, List<String> headKeys, FormulaEvaluator evaluator) {
         if (row == null) return null;
@@ -215,9 +257,9 @@ public class PoiExcelHelper {
                     value = cell.getStringCellValue();
                     break;
                 case NUMERIC:
-                    if(HSSFDateUtil.isCellDateFormatted(cell)){
+                    if (HSSFDateUtil.isCellDateFormatted(cell)) {
                         value = HSSFDateUtil.getJavaDate(cell.getNumericCellValue());
-                    }else {
+                    } else {
                         value = cell.getNumericCellValue();
                     }
                     break;
@@ -228,9 +270,66 @@ public class PoiExcelHelper {
                     CellValue cellValue = evaluator.evaluate(cell);
                     value = cellValue.getCellTypeEnum() == CellType.NUMERIC ? cellValue.getNumberValue() : cellValue.getStringValue();
                     break;
+                default:
+                    break;
+            }
+
+            if (value != null && Objects.equals(value.getClass().getName(), "java.lang.Double") && StringUtils.indexOf(value.toString(), "E") >= 0) {
+                value = DECIMAL_FORMATE.format(value);
             }
             line.put(key, value);
         }
         return line;
+    }
+
+    /**
+     * 向工作表中填充数据
+     *
+     * @param sheet      工作表
+     * @param list       数据源
+     * @param fieldMap   中英文字段对应关系的Map
+     * @param firstIndex 开始索引
+     * @param lastIndex  结束索引
+     */
+    private static void fillSheet(Sheet sheet, List<Map> list, LinkedHashMap<String, String> fieldMap, int firstIndex, int lastIndex) throws Exception {
+
+        //定义存放英文字段名和中文字段名的数组
+        String[] cnFields = new String[fieldMap.size()];
+        String[] enFields = new String[fieldMap.size()];
+
+        //填充数组
+        int count = 0;
+        for (Map.Entry<String, String> entry : fieldMap.entrySet()) {
+            cnFields[count] = entry.getKey();
+            enFields[count] = entry.getValue();
+            count++;
+        }
+        //填充表头
+        Row headRow = sheet.createRow(0);
+        CellStyle headStyle = sheet.getWorkbook().createCellStyle();
+        headStyle.setFillBackgroundColor((short) 44);
+
+        for (int i = 0; i < cnFields.length; i++) {
+            Cell cell = headRow.createCell(i);
+            cell.setCellStyle(headStyle);
+            headRow.createCell(i).setCellValue(cnFields[i]);
+        }
+
+        //填充内容
+        int rowNo = 1;
+        for (int index = firstIndex; index <= lastIndex; index++) {
+            Row row = sheet.createRow(rowNo);
+            //获取单个对象
+            Map item = list.get(index);
+            for (int i = 0; i < enFields.length; i++) {
+                Object objValue = item.get(enFields[i]);
+                if (objValue == null) continue;
+                if (objValue instanceof Date) {
+                    objValue = DateHelper.format((Date) objValue, "yyyy-MM-dd HH:mm:ss");
+                }
+                row.createCell(i).setCellValue(objValue.toString());
+            }
+            rowNo++;
+        }
     }
 }
