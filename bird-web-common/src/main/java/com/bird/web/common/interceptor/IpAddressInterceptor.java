0e42eeb017bc5345847d04166960406397d88a93
liuxx001 <381334617@qq.com>
2019-04-28 18:06:48 +0800

    添加ResponseBodyAdvice

--- bird-web-common/src/main/java/com/bird/web/common/interceptor/IpAddressInterceptor.java
+++ bird-web-common/src/main/java/com/bird/web/common/interceptor/IpAddressInterceptor.java
@@ -3,6 +3,7 @@ package com.bird.web.common.interceptor;
 import com.bird.web.common.interceptor.support.IIpAddressChecker;
 import com.bird.web.common.utils.RequestHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +21,8 @@ public class IpAddressInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (!(handler instanceof HandlerMethod)) return true;
+
         if (ipChecker == null) return true;
 
         String ip = RequestHelper.getRealIp(request);
