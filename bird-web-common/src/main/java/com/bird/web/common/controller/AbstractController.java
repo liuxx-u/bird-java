884774359d1eddf3785aafe639a74e1d0ee8f8cc
liuxx001 <381334617@qq.com>
2019-05-15 18:43:04 +0800

    基类控制器不再处理异常信息，交由业务系统通过@ControllerAdvice统一处理

--- bird-web-common/src/main/java/com/bird/web/common/controller/AbstractController.java
+++ bird-web-common/src/main/java/com/bird/web/common/controller/AbstractController.java
@@ -1,21 +1,13 @@
 package com.bird.web.common.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.bird.core.HttpCode;
-import com.bird.core.OperationResult;
-import com.bird.core.exception.AbstractException;
 import com.bird.core.session.BirdSession;
 import com.bird.core.session.SessionContext;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.Serializable;
 
 /**
  * @author liuxx
@@ -50,27 +42,5 @@ public abstract class AbstractController {
      */
     protected Long getUserId() {
         return SessionContext.getUserId();
-    }
-
-    /**
-     * 异常处理
-     */
-    @ExceptionHandler(Exception.class)
-    public void exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) throws Exception {
-        OperationResult result = new OperationResult();
-
-        if (ex instanceof AbstractException) {
-            logger.warn(ex.getMessage(), ex);
-            result = OperationResult.Fail((AbstractException) ex);
-        } else {
-            logger.error(ex.getMessage(), ex);
-            result.setCode(HttpCode.INTERNAL_SERVER_ERROR.value());
-            String msg = StringUtils.defaultIfBlank(ex.getMessage(), HttpCode.INTERNAL_SERVER_ERROR.msg());
-            result.setMessage(msg.length() > 100 ? "系统走神了,请稍候再试." : msg);
-        }
-        response.setContentType("application/json;charset=UTF-8");
-        logger.info(JSON.toJSONString(result));
-        byte[] bytes = JSON.toJSONBytes(result, SerializerFeature.DisableCircularReferenceDetect);
-        response.getOutputStream().write(bytes);
     }
 }
