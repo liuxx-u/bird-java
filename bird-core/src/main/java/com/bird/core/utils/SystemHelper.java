ce1a5d11c43b76dadc772f533271704bc2f6f158
liuxx001 <381334617@qq.com>
2019-05-17 17:00:27 +0800

    添加IpFirstLoadBalance，方便测试

---
+++ bird-core/src/main/java/com/bird/core/utils/SystemHelper.java
@@ -0,0 +1,40 @@
+package com.bird.core.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+/**
+ * @author liuxx
+ * @date 2019/5/17
+ */
+@Slf4j
+public class SystemHelper {
+
+    public static List<String> getLocalIps() {
+        List<String> ips = new ArrayList<>();
+        try {
+            Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            InetAddress ip;
+            while (allNetInterfaces.hasMoreElements()) {
+                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+                Enumeration addresses = netInterface.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    ip = (InetAddress) addresses.nextElement();
+                    if (ip instanceof Inet4Address) {
+                        ips.add(ip.getHostAddress());
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            log.warn("robbin负载均衡获取本机ip失败", ex);
+        }
+        return ips;
+    }
+}
