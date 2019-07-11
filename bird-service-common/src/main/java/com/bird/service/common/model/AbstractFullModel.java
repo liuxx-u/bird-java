c35ce2653f9695a6d6b7cc8d19c1067c72b4bf1b
liuxx001 <381334617@qq.com>
2019-07-11 10:32:43 +0800

    新增记录创建者id、修改者id的实体

---
+++ bird-service-common/src/main/java/com/bird/service/common/model/AbstractFullModel.java
@@ -0,0 +1,35 @@
+package com.bird.service.common.model;
+
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.enums.FieldFill;
+
+/**
+ * @author liuxx
+ * @date 2019/7/11
+ */
+public class AbstractFullModel extends AbstractModel implements IHasCreatorId,IHasModifierId {
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long creatorId;
+
+    @TableField(fill = FieldFill.UPDATE)
+    private Long modifierId;
+
+    @Override
+    public Long getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(Long creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    @Override
+    public Long getModifierId() {
+        return modifierId;
+    }
+
+    public void setModifierId(Long modifierId) {
+        this.modifierId = modifierId;
+    }
+}
---
+++ bird-service-common/src/main/java/com/bird/service/common/model/IHasModifierId.java
@@ -0,0 +1,15 @@
+package com.bird.service.common.model;
+
+/**
+ * @author liuxx
+ * @date 2019/7/11
+ */
+public interface IHasModifierId {
+
+    /**
+     * 获取修改者id
+     *
+     * @return id
+     */
+    Long getModifierId();
+}
