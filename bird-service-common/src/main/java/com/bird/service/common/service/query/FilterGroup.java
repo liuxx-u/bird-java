ca83caa20d9e0afdc557c4bfaf4b1fd2a0c9d0ab
liuxx001 <381334617@qq.com>
2019-07-24 17:19:11 +0800

    支持FilterGroup内部解析为SQL

--- bird-service-common/src/main/java/com/bird/service/common/service/query/FilterGroup.java
+++ bird-service-common/src/main/java/com/bird/service/common/service/query/FilterGroup.java
@@ -1,9 +1,14 @@
 package com.bird.service.common.service.query;
 
 import lombok.Getter;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 筛选条件组
@@ -39,5 +44,114 @@ public class FilterGroup implements Serializable {
     public void or(List<FilterGroup> groups){
         this.operate = FilterOperate.OR.getValue();
         this.groups = groups;
+    }
+
+    /**
+     * 解析为SQL语句
+     * @return sql
+     */
+    public String formatSQL(){
+        return this.formatSQL(new HashMap<>());
+    }
+
+    /**
+     * 解析为SQL语句
+     *
+     * @param fieldNameMap map
+     * @return sql
+     */
+    public String formatSQL(Map<String,String> fieldNameMap){
+        String where = formatRules(this.rules,fieldNameMap);
+
+        boolean isStart = true;
+        StringBuilder groupBuilder = new StringBuilder();
+        if (CollectionUtils.isNotEmpty(this.groups)) {
+            for (FilterGroup innerGroup : this.groups) {
+                if(innerGroup == null) continue;
+                String innerWhere = innerGroup.formatSQL(fieldNameMap);
+                if (StringUtils.isNotBlank(innerWhere)) {
+                    if (!isStart) {
+                        groupBuilder.append(" and ");
+                    }
+                    groupBuilder.append(innerWhere);
+                    isStart = false;
+                }
+            }
+        }
+        String groupWhere = groupBuilder.toString();
+
+        if (StringUtils.isBlank(groupWhere)) return where;
+        else if (StringUtils.isBlank(where)) return groupWhere;
+        else {
+            StringBuilder sb = new StringBuilder();
+            FilterOperate groupOperate = FilterOperate.resolveOrDefault(this.operate, FilterOperate.AND);
+            if (groupOperate == null) {
+                groupOperate = FilterOperate.AND;
+            }
+
+            sb.append("(").append(where).append(" ").append(groupOperate.getDbValue()).append(" ").append(groupWhere).append(")");
+            return sb.toString();
+        }
+    }
+
+    private String formatRules(List<FilterRule> rules, Map<String,String> fieldNameMap) {
+        if(CollectionUtils.isEmpty(rules))return StringUtils.EMPTY;
+
+        StringBuilder sb = new StringBuilder();
+
+        boolean isStart = true;
+        for (FilterRule rule : rules) {
+            if(rule == null)continue;
+
+            String field = StringUtils.strip(rule.getField());
+            String value = StringUtils.strip(rule.getValue());
+            if (StringUtils.isBlank(field) || StringUtils.isBlank(value)) continue;
+            if (!isStart) {
+                sb.append(" and ");
+            }
+
+            FilterOperate ruleOperate = FilterOperate.resolve(rule.getOperate());
+            if (ruleOperate == null) {
+                ruleOperate = FilterOperate.EQUAL;
+            }
+
+            String dbFieldName = this.getDbFieldName(fieldNameMap,field);
+
+            if (ruleOperate == FilterOperate.IN) {
+                sb.append(String.format("FIND_IN_SET(%s,%s)", dbFieldName, StringUtils.wrapIfMissing(StringUtils.strip(value, ","), "'")));
+            } else {
+                switch (ruleOperate) {
+                    case START_WITH:
+                        value = value + "%";
+                        break;
+                    case END_WITH:
+                        value = "%" + value;
+                        break;
+                    case CONTAINS:
+                        value = "%" + value + "%";
+                        break;
+                    default:
+                        break;
+                }
+                value = StringUtils.wrapIfMissing(value, "'");
+                sb.append(dbFieldName).append(" ").append(ruleOperate.getDbValue()).append(" ").append(value);
+            }
+            isStart = false;
+        }
+        String where = sb.toString();
+        return StringUtils.isNotBlank(where) ? "(" + where + ")" : where;
+    }
+
+    /**
+     * 获取字段对应的数据库列名
+     * @param fieldNameMap map
+     * @param field field
+     * @return db field name
+     */
+    private String getDbFieldName(Map<String,String> fieldNameMap,String field){
+        if(MapUtils.isEmpty(fieldNameMap)){
+            return field;
+        }
+        return fieldNameMap.getOrDefault(field,field);
     }
 }
