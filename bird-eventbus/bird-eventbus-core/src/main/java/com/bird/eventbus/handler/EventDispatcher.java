b496bb9df8bd2297377e0e85a3045fd5fef0857e
liuxx001 <381334617@qq.com>
2019-09-21 22:22:20 +0800

    修复Eventbus单消费者卡住的BUG

--- bird-eventbus/bird-eventbus-core/src/main/java/com/bird/eventbus/handler/EventDispatcher.java
+++ bird-eventbus/bird-eventbus-core/src/main/java/com/bird/eventbus/handler/EventDispatcher.java
@@ -34,24 +34,20 @@ public class EventDispatcher {
 
     @Value("${spring.application.name:}")
     private String application;
-
-    /**
-     * 事件队列
-     */
-    private BlockingQueue<IEventArg> eventQueue = new LinkedBlockingQueue<>();
     /**
      * 消费结果队列
      */
     private BlockingQueue<EventHandleResult> resultQueue = new LinkedBlockingQueue<>();
+
+    /**
+     * 事件处理线程池
+     */
+    private ExecutorService executor = Executors.newCachedThreadPool();
+
     /**
      * topic与处理方法映射关系
      */
     private final static ConcurrentMap<String, Set<Method>> EVENT_HANDLER_CONTAINER = new ConcurrentHashMap<>();
-
-    public EventDispatcher() {
-        ExecutorService executor = Executors.newCachedThreadPool();
-        executor.execute(new EventConsumer());
-    }
 
     @PostConstruct
     public void initHandlerStoreThread() {
@@ -67,15 +63,11 @@ public class EventDispatcher {
      * @param eventArg 事件信息
      */
     public void enqueue(IEventArg eventArg) {
-        if (eventArg == null){
+        if (eventArg == null) {
             log.warn("接受到事件为null");
             return;
         }
-        try {
-            eventQueue.put(eventArg);
-        } catch (InterruptedException ex) {
-            log.error("事件入队失败", ex);
-        }
+        executor.submit(() -> this.handleEvent(eventArg));
     }
 
     /**
@@ -135,136 +127,122 @@ public class EventDispatcher {
         return keys.toArray(new String[0]);
     }
 
-    /**
-     * 事件消费者
-     */
-    private class EventConsumer implements Runnable {
-        @Override
-        public void run() {
-            while (true) {
-                IEventArg eventArg;
-                try {
-                    eventArg = eventQueue.take();
-                } catch (Exception ignore) {
-                    continue;
-                }
-                if (eventArg == null) continue;
+    private void handleEvent(IEventArg eventArg) {
+        if (eventArg == null) return;
 
-                EventHandleResult handleResult = new EventHandleResult(eventArg);
-                handleResult.setGroup(application);
-                EventHandleStatusEnum status = EventHandleStatusEnum.FAIL;
-                int span = 24 * 60 * 60 * 1000;
-                if (System.currentTimeMillis() - eventArg.getEventTime().getTime() > span) {
-                    status = EventHandleStatusEnum.TIMEOUT;
-                } else {
-                    String eventKey = eventArg.getClass().getName();
-                    Set<Method> methods = EVENT_HANDLER_CONTAINER.getOrDefault(eventKey, null);
-                    if (!CollectionUtils.isEmpty(methods)) {
-                        int successCount = 0;
-                        for (Method method : methods) {
-                            EventHandleResult.ConsumerResult itemResult = this.invokeMethod(method, eventArg, handleResult);
-                            handleResult.addItem(itemResult);
-                            if (itemResult.getSuccess()) {
-                                successCount++;
-                            }
-                        }
-                        if (successCount >= handleResult.getItems().size()) {
-                            status = EventHandleStatusEnum.SUCCESS;
-                        } else if (successCount > 0) {
-                            status = EventHandleStatusEnum.PARTIAL_SUCCESS;
-                        }
+        EventHandleResult handleResult = new EventHandleResult(eventArg);
+        handleResult.setGroup(application);
+        EventHandleStatusEnum status = EventHandleStatusEnum.FAIL;
+        int span = 24 * 60 * 60 * 1000;
+        if (System.currentTimeMillis() - eventArg.getEventTime().getTime() > span) {
+            status = EventHandleStatusEnum.TIMEOUT;
+        } else {
+            String eventKey = eventArg.getClass().getName();
+            Set<Method> methods = EVENT_HANDLER_CONTAINER.getOrDefault(eventKey, null);
+            if (!CollectionUtils.isEmpty(methods)) {
+                int successCount = 0;
+                for (Method method : methods) {
+                    EventHandleResult.ConsumerResult itemResult = this.invokeMethod(method, eventArg, handleResult);
+                    handleResult.addItem(itemResult);
+                    if (itemResult.getSuccess()) {
+                        successCount++;
                     }
                 }
-                handleResult.setStatus(status);
-                if (handlerStore != null) {
-                    try {
-                        resultQueue.put(handleResult);
-                    } catch (InterruptedException ex) {
-                        log.error("事件消费结果入队失败", ex);
-                    }
+                if (successCount >= handleResult.getItems().size()) {
+                    status = EventHandleStatusEnum.SUCCESS;
+                } else if (successCount > 0) {
+                    status = EventHandleStatusEnum.PARTIAL_SUCCESS;
                 }
             }
         }
-
-        /**
-         * 执行事件处理方法
-         *
-         * @param method   处理程序方法
-         * @param eventArg 事件参数
-         */
-        private EventHandleResult.ConsumerResult invokeMethod(Method method, IEventArg eventArg, EventHandleResult handleResult) {
-
-            EventHandleResult.ConsumerResult consumerResult = handleResult.new ConsumerResult();
-
-            Class typeClass = method.getDeclaringClass();
+        handleResult.setStatus(status);
+        if (handlerStore != null) {
             try {
-                Object instance = applicationContext.getBean(typeClass);
-
-                this.interceptBefore(method, eventArg);
-
-                method.invoke(instance, eventArg);
-
-                this.interceptAfter(method, eventArg);
-                consumerResult.setSuccess(true);
-            } catch (InvocationTargetException e) {
-                this.interceptException(method, eventArg, e);
-
-                consumerResult.setSuccess(false);
-                consumerResult.setMessage(e.getTargetException().getMessage());
-                log.error("事件消费失败", e);
-            } catch (Exception e) {
-                this.interceptException(method, eventArg, e);
-
-                consumerResult.setSuccess(false);
-                consumerResult.setMessage(e.getLocalizedMessage());
-                log.error("事件消费失败", e);
-            }
-
-            consumerResult.setClazz(typeClass.getName());
-            consumerResult.setMethod(method.getName());
-            return consumerResult;
-        }
-
-
-        /**
-         * 执行事件处理前拦截方法
-         *
-         * @param method   执行的方法
-         * @param eventArg 事件消息
-         */
-        private void interceptBefore(Method method, IEventArg eventArg) {
-            if (interceptors != null) {
-                for (IEventHandlerInterceptor interceptor : interceptors) {
-                    interceptor.beforeHandle(eventArg, method);
-                }
+                resultQueue.put(handleResult);
+            } catch (InterruptedException ex) {
+                log.error("事件消费结果入队失败", ex);
             }
         }
+    }
 
-        /**
-         * 执行事件处理后拦截方法
-         *
-         * @param method   执行的方法
-         * @param eventArg 事件消息
-         */
-        private void interceptAfter(Method method, IEventArg eventArg) {
-            if (interceptors != null) {
-                for (IEventHandlerInterceptor interceptor : interceptors) {
-                    interceptor.afterHandle(eventArg, method);
-                }
-            }
+    /**
+     * 执行事件处理方法
+     *
+     * @param method   处理程序方法
+     * @param eventArg 事件参数
+     */
+    private EventHandleResult.ConsumerResult invokeMethod(Method method, IEventArg eventArg, EventHandleResult handleResult) {
+
+        EventHandleResult.ConsumerResult consumerResult = handleResult.new ConsumerResult();
+
+        Class typeClass = method.getDeclaringClass();
+        try {
+            Object instance = applicationContext.getBean(typeClass);
+
+            this.interceptBefore(method, eventArg);
+
+            method.invoke(instance, eventArg);
+
+            this.interceptAfter(method, eventArg);
+            consumerResult.setSuccess(true);
+        } catch (InvocationTargetException e) {
+            this.interceptException(method, eventArg, e);
+
+            consumerResult.setSuccess(false);
+            consumerResult.setMessage(e.getTargetException().getMessage());
+            log.error("事件消费失败", e);
+        } catch (Exception e) {
+            this.interceptException(method, eventArg, e);
+
+            consumerResult.setSuccess(false);
+            consumerResult.setMessage(e.getLocalizedMessage());
+            log.error("事件消费失败", e);
         }
 
-        /**
-         * 执行事件处理异常拦截方法
-         *
-         * @param method   执行的方法
-         * @param eventArg 事件消息
-         */
-        private void interceptException(Method method, IEventArg eventArg, Exception ex) {
-            if (interceptors != null) {
-                for (IEventHandlerInterceptor interceptor : interceptors) {
-                    interceptor.onException(eventArg, method, ex);
-                }
+        consumerResult.setClazz(typeClass.getName());
+        consumerResult.setMethod(method.getName());
+        return consumerResult;
+    }
+
+
+    /**
+     * 执行事件处理前拦截方法
+     *
+     * @param method   执行的方法
+     * @param eventArg 事件消息
+     */
+    private void interceptBefore(Method method, IEventArg eventArg) {
+        if (interceptors != null) {
+            for (IEventHandlerInterceptor interceptor : interceptors) {
+                interceptor.beforeHandle(eventArg, method);
+            }
+        }
+    }
+
+    /**
+     * 执行事件处理后拦截方法
+     *
+     * @param method   执行的方法
+     * @param eventArg 事件消息
+     */
+    private void interceptAfter(Method method, IEventArg eventArg) {
+        if (interceptors != null) {
+            for (IEventHandlerInterceptor interceptor : interceptors) {
+                interceptor.afterHandle(eventArg, method);
+            }
+        }
+    }
+
+    /**
+     * 执行事件处理异常拦截方法
+     *
+     * @param method   执行的方法
+     * @param eventArg 事件消息
+     */
+    private void interceptException(Method method, IEventArg eventArg, Exception ex) {
+        if (interceptors != null) {
+            for (IEventHandlerInterceptor interceptor : interceptors) {
+                interceptor.onException(eventArg, method, ex);
             }
         }
     }
