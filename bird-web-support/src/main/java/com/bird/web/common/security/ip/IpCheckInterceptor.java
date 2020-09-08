4ac38981c578912323fa05c305fc3ce94efd24d7
liuxx001 <381334617@qq.com>
2020-09-08 15:26:12 +0800

    ip白名单拦截器，支持ip段的控制

--- bird-web-support/src/main/java/com/bird/web/common/security/ip/IpCheckInterceptor.java
+++ bird-web-support/src/main/java/com/bird/web/common/security/ip/IpCheckInterceptor.java
@@ -1,6 +1,7 @@
 package com.bird.web.common.security.ip;
 
 import com.bird.core.SpringContextHolder;
+import com.bird.web.common.utils.IpHelper;
 import com.bird.web.common.utils.RequestHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.AntPathMatcher;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * ip规则校验拦截器
@@ -28,7 +30,7 @@ public class IpCheckInterceptor extends HandlerInterceptorAdapter {
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         if (!(handler instanceof HandlerMethod)) {
             return true;
         }
@@ -38,18 +40,23 @@ public class IpCheckInterceptor extends HandlerInterceptorAdapter {
 
         String uri = request.getRequestURI();
         PathMatcher pathMatcher = this.getPathMatcher();
+        boolean isMatchUri = false;
         for (IpConfProperties ipConf : ipListProvider.listIps()) {
             if (pathMatcher.match(ipConf.getUriPattern(), uri)) {
+                isMatchUri = true;
                 String ip = RequestHelper.getRealIp(request);
                 for (String ipPattern : ipConf.listIps()) {
-                    //TODO: Check ip pattern
-
+                    if (IpHelper.checkIpRange(ipPattern, ip)) {
+                        return true;
+                    }
                 }
-                return false;
             }
         }
-
-        return true;
+        if (!isMatchUri) {
+            return true;
+        }
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, "ip访问限制");
+        return false;
     }
 
     private PathMatcher getPathMatcher() {
--- bird-web-support/src/main/java/com/bird/web/common/utils/IpHelper.java
+++ bird-web-support/src/main/java/com/bird/web/common/utils/IpHelper.java
@@ -1,10 +1,57 @@
 package com.bird.web.common.utils;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
 /**
  * @author liuxx
  * @since 2020/9/4
  */
 public final class IpHelper {
+
+    private final static int IP_MAX_LENGTH = 32;
+    private final static int SEGMENT_COUNT = 4;
+    private final static int MAX_SEGMENT_VALUE = 255;
+    private final static String IP_DELIMITER_REGEX = "\\.";
+    private final static String SEGMENT_DELIMITER_REGEX = "/";
+    private final static String DEFAULT_SELF_SEGMENT = "0";
+
+    /**
+     * 校验ip是否在指定的ip段之内
+     *
+     * @param range ip段，x.x.x.x/x
+     * @param ip    指定ip
+     * @return 是否在ip范围内
+     */
+    public static boolean checkIpRange(String range, String ip) {
+        if (!checkIp4Address(ip)) {
+            return false;
+        }
+
+        String startIp = getStartIp(range);
+        String endIp = getEndIp(range);
+        if (Objects.equals(startIp, ip) || Objects.equals(endIp, ip)) {
+            return true;
+        } else if (Objects.equals(startIp, endIp)) {
+            return false;
+        }
+
+        Integer[] startIpArray = getIpArray(startIp);
+        Integer[] endIpArray = getIpArray(endIp);
+        if (startIpArray.length != SEGMENT_COUNT || endIpArray.length != SEGMENT_COUNT) {
+            return false;
+        }
+        Integer[] ipArray = getIpArray(ip);
+        for (int i = 0; i < SEGMENT_COUNT; i++) {
+            if (ipArray[i] > startIpArray[i] && ipArray[i] < endIpArray[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     /**
      * 根据掩码位数计算掩码
@@ -18,27 +65,26 @@ public final class IpHelper {
         try {
             netMask = Integer.parseInt(maskIndex);
         } catch (NumberFormatException e) {
-            System.out.println(e.getMessage());
             return null;
         }
-        if (netMask > 32) {
+        if (netMask > IP_MAX_LENGTH) {
             return null;
         }
         // 子网掩码为1占了几个字节
         int num1 = netMask / 8;
         // 子网掩码的补位位数
         int num2 = netMask % 8;
-        int[] array = new int[4];
+        int[] array = new int[SEGMENT_COUNT];
         for (int i = 0; i < num1; i++) {
             array[i] = 255;
         }
-        for (int i = num1; i < 4; i++) {
+        for (int i = num1; i < SEGMENT_COUNT; i++) {
             array[i] = 0;
         }
         for (int i = 0; i < num2; num2--) {
             array[num1] += 1 << 8 - num2;
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < SEGMENT_COUNT; i++) {
             if (i == 3) {
                 mask.append(array[i]);
             } else {
@@ -46,10 +92,6 @@ public final class IpHelper {
             }
         }
         return mask.toString();
-    }
-
-    public static void main(String[] args) {
-        System.out.println(getNetMask("27"));
     }
 
     /**
@@ -60,35 +102,36 @@ public final class IpHelper {
      * @param segment 网段
      * @return 起始IP
      */
-    public static String getStartIp(String segment) {
-        StringBuilder startIp = new StringBuilder();
-        if (segment == null) {
+    private static String getStartIp(String segment) {
+        if (StringUtils.isBlank(segment)) {
             return null;
         }
-        String[] arr = segment.split("/");
+        String[] arr = segment.split(SEGMENT_DELIMITER_REGEX);
         String ip = arr[0];
-        String maskIndex = arr[1];
-        String mask = IpHelper.getNetMask(maskIndex);
-        if (4 != ip.split("\\.").length || mask == null) {
+        if (!checkIp4Address(ip)) {
             return null;
         }
-        int[] ipArray = new int[4];
-        int[] netMaskArray = new int[4];
-        for (int i = 0; i < 4; i++) {
-            try {
-                ipArray[i] = Integer.parseInt(ip.split("\\.")[i]);
-                netMaskArray[i] = Integer.parseInt(mask.split("\\.")[i]);
-                if (ipArray[i] > 255 || ipArray[i] < 0 || netMaskArray[i] > 255 || netMaskArray[i] < 0) {
-                    return null;
-                }
-                ipArray[i] = ipArray[i] & netMaskArray[i];
-                if (i == 3) {
-                    startIp.append(ipArray[i] + 1);
-                } else {
-                    startIp.append(ipArray[i]).append(".");
-                }
-            } catch (NumberFormatException e) {
-                System.out.println(e.getMessage());
+        if (arr.length == 1) {
+            return ip;
+        }
+
+        String maskIndex = arr[1];
+        if (Objects.equals(maskIndex, DEFAULT_SELF_SEGMENT)) {
+            return ip;
+        }
+        String mask = IpHelper.getNetMask(maskIndex);
+        if (mask == null || !checkIp4Address(mask)) {
+            return ip;
+        }
+        Integer[] ipArray = getIpArray(ip);
+        Integer[] netMaskArray = getIpArray(mask);
+        StringBuilder startIp = new StringBuilder();
+        for (int i = 0; i < SEGMENT_COUNT; i++) {
+            ipArray[i] = ipArray[i] & netMaskArray[i];
+            if (i == SEGMENT_COUNT - 1) {
+                startIp.append(ipArray[i] + 1);
+            } else {
+                startIp.append(ipArray[i]).append(".");
             }
         }
         return startIp.toString();
@@ -97,60 +140,90 @@ public final class IpHelper {
     /**
      * 根据网段计算结束IP
      *
-     * @param segment
+     * @param segment 网段
      * @return 结束IP
      */
-    public static String getEndIp(String segment) {
-        StringBuilder endIp = new StringBuilder();
+    private static String getEndIp(String segment) {
         String startIp = getStartIp(segment);
-        if (segment == null) {
+        if (startIp == null || !checkIp4Address(startIp)) {
             return null;
         }
-        String[] arr = segment.split("/");
-        String maskIndex = arr[1];
-        //实际需要的IP个数
-        int hostNumber;
-        int[] startIpArray = new int[4];
+        String[] arr = segment.split(SEGMENT_DELIMITER_REGEX);
+        if (arr.length == 1) {
+            return startIp;
+        }
+        int maskIndex;
+        Integer[] ipArray;
         try {
-            hostNumber = 1 << 32 - (Integer.parseInt(maskIndex));
-            for (int i = 0; i < 4; i++) {
-                startIpArray[i] = Integer.parseInt(startIp.split("\\.")[i]);
-                if (i == 3) {
-                    startIpArray[i] = startIpArray[i] - 1;
-                    break;
-                }
-            }
-            startIpArray[3] = startIpArray[3] + (hostNumber - 1);
+            maskIndex = Integer.parseInt(arr[1]);
+            ipArray = getIpArray(startIp);
         } catch (NumberFormatException e) {
-            System.out.println(e.getMessage());
+            return startIp;
         }
 
-        if (startIpArray[3] > 255) {
+        //实际需要的IP个数
+        int hostNumber;
+        int[] startIpArray = new int[SEGMENT_COUNT];
+        hostNumber = 1 << IP_MAX_LENGTH - (maskIndex);
+        for (int i = 0; i < SEGMENT_COUNT; i++) {
+            startIpArray[i] = ipArray[i];
+            if (i == 3) {
+                startIpArray[i] = startIpArray[i] - 1;
+                break;
+            }
+        }
+        startIpArray[3] = startIpArray[3] + (hostNumber - 1);
+
+        if (startIpArray[3] > MAX_SEGMENT_VALUE) {
             int k = startIpArray[3] / 256;
             startIpArray[3] = startIpArray[3] % 256;
             startIpArray[2] = startIpArray[2] + k;
         }
-        if (startIpArray[2] > 255) {
+        if (startIpArray[2] > MAX_SEGMENT_VALUE) {
             int j = startIpArray[2] / 256;
             startIpArray[2] = startIpArray[2] % 256;
             startIpArray[1] = startIpArray[1] + j;
-            if (startIpArray[1] > 255) {
+            if (startIpArray[1] > MAX_SEGMENT_VALUE) {
                 int k = startIpArray[1] / 256;
                 startIpArray[1] = startIpArray[1] % 256;
                 startIpArray[0] = startIpArray[0] + k;
             }
         }
-        for (int i = 0; i < 4; i++) {
-            if (i == 3) {
-                startIpArray[i] = startIpArray[i] - 1;
-            }
-            if ("".equals(endIp.toString()) || endIp.length() == 0) {
-                endIp.append(startIpArray[i]);
+        StringBuilder endIp = new StringBuilder();
+        for (int i = 0; i < SEGMENT_COUNT; i++) {
+            if (i == SEGMENT_COUNT - 1) {
+                endIp.append(startIpArray[i] - 1);
             } else {
-                endIp.append(".").append(startIpArray[i]);
+                endIp.append(ipArray[i]).append(".");
             }
         }
         return endIp.toString();
     }
 
+    /**
+     * 校验IP格式是否正确
+     *
+     * @param ipAddress ipv4地址
+     * @return 是否合法的ip
+     */
+    private static Boolean checkIp4Address(String ipAddress) {
+        if (StringUtils.isBlank(ipAddress)) {
+            return false;
+        }
+        String regex = "^((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})(\\.((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})){3}$";
+        return Pattern.matches(regex, ipAddress);
+    }
+
+    /**
+     * ip地址解析为十进制数组
+     *
+     * @param ipAddress ipv4地址
+     * @return ip数组
+     */
+    private static Integer[] getIpArray(String ipAddress) {
+        if (StringUtils.isBlank(ipAddress)) {
+            return new Integer[0];
+        }
+        return Arrays.stream(ipAddress.split(IP_DELIMITER_REGEX)).map(Integer::parseInt).toArray(Integer[]::new);
+    }
 }
