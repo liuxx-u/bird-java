4632549defa8ffbb121bccc1e39f7b2ff7a3cd41
liuxx001 <381334617@qq.com>
2020-09-29 13:59:13 +0800

    修改ID生成器规则：时间戳 + 机器ID + jvmPid + 自增序列

--- bird-service-support/src/main/java/com/bird/service/common/incrementer/UUIDHexGenerator.java
+++ bird-service-support/src/main/java/com/bird/service/common/incrementer/UUIDHexGenerator.java
@@ -1,79 +1,166 @@
 package com.bird.service.common.incrementer;
 
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.management.ManagementFactory;
 import java.net.InetAddress;
-import java.util.Random;
+import java.net.NetworkInterface;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author liuxx
  * @date 2019/8/23
  */
+@Slf4j
 public final class UUIDHexGenerator {
 
     private final static int RADIX = 36;
+    private final static int TIME_BACK_OFFSET = 1000;
     private final static String DELIMITER = "-";
-    private final static String FORMAT_IP;
-    private final static Random RANDOM = new Random();
+    private final static String MACHINE_ID = formatMachineId();
+    private final static String JVM_PID = formatJvmPid();
 
-    static {
-        int ipAddress;
-        try {
-            ipAddress = Math.abs(toInt(InetAddress.getLocalHost().getAddress()));
-        } catch (Exception e) {
-            ipAddress = 0;
-        }
-        FORMAT_IP = format(ipAddress);
-    }
+    private static volatile Short SEQUENCE = 0;
+    private static volatile long LAST_TIME_STAMP = timeGen();
 
     private UUIDHexGenerator() {
     }
 
-    private static int counter = 0;
+    /**
+     * 获取下一个 ID
+     *
+     * @return 下一个 ID
+     */
+    public synchronized static String generate() {
+        long timestamp = timeGen();
+        //闰秒
+        if (timestamp < LAST_TIME_STAMP) {
+            long offset = LAST_TIME_STAMP - timestamp;
+            if (offset <= TIME_BACK_OFFSET) {
+                try {
+                    Thread.sleep(offset);
+                    timestamp = timeGen();
+                } catch (Exception e) {
+                    throw new RuntimeException(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", offset), e);
+                }
+            } else {
+                throw new RuntimeException(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", offset));
+            }
+        }
 
-    public static String generate() {
-        return format(System.currentTimeMillis()) + DELIMITER
-                + FORMAT_IP + DELIMITER
-                + format(Math.abs(RANDOM.nextInt())) + DELIMITER
-                + format(getCount());
+        if (LAST_TIME_STAMP == timestamp) {
+            // 相同毫秒内，序列号自增
+            if (++SEQUENCE <= 0) {
+                // 同一毫秒的序列数已经达到最大
+                timestamp = tilNextMillis();
+            }
+        } else {
+            // 不同毫秒内，序列号置为 1
+            SEQUENCE = 1;
+        }
+
+        LAST_TIME_STAMP = timestamp;
+
+        // 时间戳部分 | 机器标识部分 | 进程标识部分 | 序列号部分
+        return formatMillis(timestamp) + DELIMITER
+                + MACHINE_ID + DELIMITER
+                + JVM_PID + DELIMITER
+                + formatSequence(SEQUENCE);
     }
 
-    private static String format(long longValue) {
-        String formatted = Long.toString(longValue, RADIX);
+    /**
+     * 获取时间戳部分
+     */
+    private static String formatMillis(long timestamp) {
+        String formatted = Long.toString(timestamp, RADIX);
 
         StringBuilder buf = new StringBuilder("000000000");
         buf.replace(9 - formatted.length(), 9, formatted);
         return buf.toString();
     }
 
-    private static String format(int intValue) {
-        String formatted = Integer.toString(intValue, RADIX);
+    /**
+     * 获取机器标识部分
+     */
+    private static String formatMachineId() {
+        long machineId;
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+            if (network == null) {
+                machineId = toLong(ip.getAddress());
+            } else {
+                byte[] mac = network.getHardwareAddress();
+                machineId = toLong(mac);
+            }
+        } catch (Exception e) {
+            log.warn("获取机器id失败 " + e.getMessage());
+            machineId = ThreadLocalRandom.current().nextInt();
+        }
+
+        String formatted = Long.toString(machineId, RADIX);
+
+        StringBuilder buf = new StringBuilder("0000000000");
+        buf.replace(10 - formatted.length(), 10, formatted);
+        return buf.toString();
+    }
+
+    /**
+     * 获取jvmPid
+     */
+    private static String formatJvmPid() {
+        int pid = 0;
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+        if (StringUtils.isNotBlank(name)) {
+            // GET jvmPid
+            try {
+                pid = Integer.parseInt(name.split(StringPool.AT)[0]);
+            } catch (Exception ex) {
+                log.error("获取jvmPid失败", ex);
+            }
+        }
+
+        String formatted = Integer.toString(pid, RADIX);
         StringBuilder buf = new StringBuilder("000000");
         buf.replace(6 - formatted.length(), 6, formatted);
         return buf.toString();
     }
 
     /**
-     * 计数器
+     * 获取序列号部分
      */
-    private static int getCount() {
-        synchronized (UUIDHexGenerator.class) {
-            if (counter < 0) {
-                counter = 0;
-            }
-            return counter++;
-        }
+    private static String formatSequence(short sequence) {
+        String formatted = Integer.toString(sequence, RADIX);
+        StringBuilder buf = new StringBuilder("000");
+        buf.replace(3 - formatted.length(), 3, formatted);
+        return buf.toString();
     }
 
     /**
-     * 将byte数组转换为整数
+     * 将byte数组转换为整数，并取绝对值
      *
      * @param bytes byte数组
      * @return int.
      */
-    private static int toInt(byte[] bytes) {
-        int result = 0;
+    private static long toLong(byte[] bytes) {
+        long result = 0;
         for (int i = bytes.length - 1; i >= 0; i--) {
             result += bytes[i] * Math.pow(0xFF, bytes.length - i - 1);
         }
-        return result;
+        return Math.abs(result);
+    }
+
+    private static long tilNextMillis() {
+        long timestamp = timeGen();
+        while (timestamp <= LAST_TIME_STAMP) {
+            timestamp = timeGen();
+        }
+        return timestamp;
+    }
+
+    private static long timeGen() {
+        return System.currentTimeMillis();
     }
 }
