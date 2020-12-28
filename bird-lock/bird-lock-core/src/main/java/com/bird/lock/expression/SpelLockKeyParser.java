0d261780c0ae80ad50d930b58db8c290a22a640e
liuxx001 <381334617@qq.com>
2020-12-28 15:39:08 +0800

    Key解析默认注入上下文Session信息

--- bird-lock/bird-lock-core/src/main/java/com/bird/lock/expression/SpelLockKeyParser.java
+++ bird-lock/bird-lock-core/src/main/java/com/bird/lock/expression/SpelLockKeyParser.java
@@ -1,5 +1,6 @@
 package com.bird.lock.expression;
 
+import com.bird.core.session.SessionContext;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.expression.ExpressionParser;
@@ -19,6 +20,7 @@ import java.lang.reflect.Parameter;
  */
 public class SpelLockKeyParser implements ILockKeyParser {
 
+    private final static String SESSION_KEY = "session";
     private final TemplateParserContext templateParserContext = new TemplateParserContext("{", "}");
 
     @Override
@@ -29,6 +31,11 @@ public class SpelLockKeyParser implements ILockKeyParser {
 
         ExpressionParser expressionParser = new SpelExpressionParser();
         StandardEvaluationContext evaluationContext = new StandardEvaluationContext();
+
+        //默认添加当前用户的Session变量
+        evaluationContext.setVariable(SESSION_KEY, SessionContext.getSession());
+
+        //添加方法中的参数变量
         int n = Integer.min(parameters.length, args.length);
         for (int i = 0; i < n; i++) {
             evaluationContext.setVariable(parameters[i].getName(), args[i]);
